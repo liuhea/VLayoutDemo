d724b9885ca27ee8ca1c2b98744f68d541601410
liuhe <liuhe@juran.com.cn>
2018-03-06 00:06:07 +0800

    add v-layout into build.gralde

--- app/src/main/java/com/liuhe/vlayoutsample/MainActivity.kt
+++ app/src/main/java/com/liuhe/vlayoutsample/MainActivity.kt
@@ -2,11 +2,29 @@ package com.liuhe.vlayoutsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.alibaba.android.vlayout.VirtualLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import com.alibaba.android.vlayout.DelegateAdapter
+import com.alibaba.android.vlayout.VirtualLayoutAdapter
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initRcy()
+
+    }
+
+    private fun initRcy() {
+        var layoutManager = VirtualLayoutManager(this)
+        rcy.layoutManager = layoutManager
+        val delegateAdapter = DelegateAdapter(layoutManager, true)
+        rcy.adapter = delegateAdapter
+
+//        delegateAdapter.addAdapter()
+
     }
 }
